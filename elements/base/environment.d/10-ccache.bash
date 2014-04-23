df78cd3dcac5ca896cd8f537dff516b2f407db24
Derek Higgins <derekh@redhat.com>
2014-04-23 01:11:40 +0100

    Place /usr/lib64/ccache in PATH
    ccache wasn't being used on fedora as the symlinks are in lib64.
    
    Change-Id: I810558799dd2382b36b5112e435b74fd5caa8ccf

--- elements/base/environment.d/10-ccache.bash
+++ elements/base/environment.d/10-ccache.bash
@@ -1,5 +1,7 @@
 # Put ccache in path for known compilers
-if ! [[ "$PATH" =~ "/usr/lib/ccache" ]] ; then
-    export PATH=/usr/lib/ccache:$PATH
-fi
+for CCACHEDIR in "/usr/lib/ccache" "/usr/lib64/ccache" ; do
+    if ! [[ "$PATH" =~ "$CCACHEDIR" ]] ; then
+        export PATH=$CCACHEDIR:$PATH
+    fi
+done
 export CCACHE_DIR=/tmp/ccache
