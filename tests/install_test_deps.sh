9a6cf7ee6f14d252a9161dda1730e4de368149fd
Gregory Haynes <greg@greghaynes.net>
2015-05-07 21:43:18 +0000

    Add debian build test case
    Weve had some regressions recently with the changes in the debian
    element. Lets tests that we can build debian images.
    
    Change-Id: I048e7a32ecb4088ec1b1e3b1efdf146187b093db

--- tests/install_test_deps.sh
+++ tests/install_test_deps.sh
@@ -4,5 +4,5 @@ set -eux
 set -o pipefail
 
 sudo apt-get update || true
-sudo apt-get install -y qemu-utils kpartx || \
-    sudo yum -y install qemu-img kpartx
+sudo apt-get install -y qemu-utils kpartx debootstrap || \
+    sudo yum -y install qemu-img kpartx debootstrap
