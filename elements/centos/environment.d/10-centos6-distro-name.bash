097240c226857a72848726017322bda30dfe645c
Bernard Cafarelli <bcafarel@redhat.com>
2016-07-01 12:14:38 +0200

    Export YUM variable in centos bases
    Other fedora/centos elements can use the YUM variable, already set in
    some base elements (fedora, centos-minimal). This commit also exports it
    for centos/centos7.
    
    Set a fallback value in pip-and-virtualenv element.
    
    Change-Id: I681d77b924be035c81043bb34c72ec5f859e7108
    Closes-Bug: 1598087

--- elements/centos/environment.d/10-centos6-distro-name.bash
+++ elements/centos/environment.d/10-centos6-distro-name.bash
@@ -1,2 +1,5 @@
 export DISTRO_NAME=centos
 export DIB_RELEASE=${DIB_RELEASE:-GenericCloud}
+
+# Useful for elements that work with fedora (dnf) & centos
+export YUM=${YUM:-yum}
--- elements/centos7/environment.d/10-centos7-distro-name.bash
+++ elements/centos7/environment.d/10-centos7-distro-name.bash
@@ -1,2 +1,5 @@
-export DIB_RELEASE=GenericCloud
 export DISTRO_NAME=centos7
+export DIB_RELEASE=GenericCloud
+
+# Useful for elements that work with fedora (dnf) & centos
+export YUM=${YUM:-yum}
