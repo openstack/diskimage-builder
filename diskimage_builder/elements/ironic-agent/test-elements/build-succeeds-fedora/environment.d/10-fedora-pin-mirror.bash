18a0d970fa1d500c58aa378e7b3f7a176cdf3ab6
Ian Wienand <iwienand@redhat.com>
2017-06-20 21:34:57 +1000

    Move ironic-agent test to fedora-minimal
    I'm uncertain as to why this is using the "fedora" element for testing
    ... but it requires downloading the fedora .qcow on every test which
    has shown to be unreliable.  An easy thing to do is to switch it to
    fedora-minimal; that will only involve downloads from local mirrors in
    the gate.
    
    Add redhat-rpm-config for minimal.  I admit I have not fully gone
    through why this is not pulled in.  It's been an issue since
    I459f2203fa145049dda185da952813118193d573 and there's all sorts of
    bugs.
    
    Change-Id: I37458e3926dae32a259bd5aa9efc645561b029a0

--- diskimage_builder/elements/ironic-agent/test-elements/build-succeeds-fedora/environment.d/10-fedora-pin-mirror.bash
+++
@@ -1,7 +0,0 @@
-# Set DIB_DISTRIBUTION_MIRROR if running in openstack gate
-if [ -f /etc/ci/mirror_info.sh ]; then
-    source /etc/ci/mirror_info.sh
-    export DIB_DISTRIBUTION_MIRROR=$NODEPOOL_FEDORA_MIRROR
-else
-    export DIB_DISTRIBUTION_MIRROR=http://dl.fedoraproject.org/pub/fedora/linux
-fi
