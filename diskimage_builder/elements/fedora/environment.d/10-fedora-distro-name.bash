490cf3aa493e00a67c3632918733b9046a8a207d
Tim Flink <tflink@fedoraproject.org>
2018-01-23 11:41:36 +0100

    Add support for Fedora 27, remove EOL Fedora 25
    This updates diskimage-builder to support current Fedora releases (26
    and 27) and removes support for Fedora 25 which is EOL as of December
    12, 2017.
    
    Change-Id: I227a607c6c468cc8b7bb154a189e9c8ce2021192

--- diskimage_builder/elements/fedora/environment.d/10-fedora-distro-name.bash
+++ diskimage_builder/elements/fedora/environment.d/10-fedora-distro-name.bash
@@ -1,5 +1,5 @@
 export DISTRO_NAME=fedora
-export DIB_RELEASE=${DIB_RELEASE:-26}
+export DIB_RELEASE=${DIB_RELEASE:-27}
 
 # Note the filename URL has a "sub-release" in it
 #  http:// ... Fedora-Cloud-Base-25-1.3.x86_64.qcow2
@@ -11,10 +11,10 @@ export DIB_RELEASE=${DIB_RELEASE:-26}
 #
 # [1] https://lists.fedoraproject.org/archives/list/cloud@lists.fedoraproject.org/thread/2WFO2FKIGUQYRQXIR35UVJGRHF7LQENJ/
 
-if [[ ${DIB_RELEASE} == '25' ]]; then
-    export DIB_FEDORA_SUBRELEASE=1.3
-elif [[ ${DIB_RELEASE} == '26' ]]; then
+if [[ ${DIB_RELEASE} == '26' ]]; then
     export DIB_FEDORA_SUBRELEASE=1.5
+elif [[ ${DIB_RELEASE} == '27' ]]; then
+    export DIB_FEDORA_SUBRELEASE=1.6
 else
     echo "Unsupported Fedora release"
     exit 1
