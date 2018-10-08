89f95a5351078c0d2272657d236570249da381b4
Matthew Thode <mthode@mthode.org>
2018-10-08 00:39:35 -0500

    enable caching for gentoo builds
    To facilitate this I've created two new environment variables to set
    the environment and default options for package actions.
    
    eval is needed for the export as it preserves quotes.
    
    Change-Id: Ib03651ee8dacd48cd1c135afd57cd31101356056
    Signed-off-by: Matthew Thode <mthode@mthode.org>

--- diskimage_builder/elements/gentoo/environment.d/00-gentoo-envars.bash
+++ diskimage_builder/elements/gentoo/environment.d/00-gentoo-envars.bash
@@ -5,3 +5,13 @@ export GENTOO_PORTAGE_CLEANUP=${GENTOO_PORTAGE_CLEANUP:-'True'}
 export GENTOO_PYTHON_TARGETS=${GENTOO_PYTHON_TARGETS:-'python2_7 python3_5'}
 export GENTOO_PYTHON_ACTIVE_VERSION=${GENTOO_PYTHON_ACTIVE_VERSION:-'python3.5'}
 export GENTOO_OVERLAYS=${GENTOO_OVERLAYS:-''}
+export GENTOO_EMERGE_DEFAULT_OPTS=${GENTOO_EMERGE_DEFAULT_OPTS:-"--binpkg-respect-use --rebuilt-binaries=y --usepkg=y --with-bdeps=y --binpkg-changed-deps=y --quiet --jobs=2"}
+
+# set the default bash array if GENTOO_EMERGE_ENV is not defined as an array
+if ! declare -p GENTOO_EMERGE_ENV  2> /dev/null | grep -q '^declare \-a'; then
+    export GENTOO_EMERGE_ENV=('USE="-build"' 'FEATURES="binpkg-multi-instance buildpkg parallel-fetch parallel-install"')
+fi
+# itterate over the array, exporting each 'line'
+for (( i=0; i<${#GENTOO_EMERGE_ENV[@]}; i++ )); do
+    eval export "${GENTOO_EMERGE_ENV[i]}"
+done
