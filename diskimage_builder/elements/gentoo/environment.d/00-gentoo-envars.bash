f1d7e902e39264a0dd9584d7da11e7e62339d0a6
Matthew Thode <mthode@mthode.org>
2019-07-26 17:18:40 -0500

    support alternate portage directories
    The 17.1 profile changed the defaults used in portage for where we store
    our repo, distfiles and binpkgs.  Some portage related variables need to
    be set deterministically.  17.1 is no enabled for Systemd's profile.
    
    Change-Id: Ib55f6875c5cb461c3c530b51d7420ce3dc8da360

--- diskimage_builder/elements/gentoo/environment.d/00-gentoo-envars.bash
+++ diskimage_builder/elements/gentoo/environment.d/00-gentoo-envars.bash
@@ -1,15 +1,21 @@
 export DIB_RELEASE=gentoo
 export DISTRO_NAME=gentoo
-export GENTOO_PROFILE=${GENTOO_PROFILE:-'default/linux/amd64/17.0'}
+export GENTOO_PROFILE=${GENTOO_PROFILE:-'default/linux/amd64/17.1'}
 export GENTOO_PORTAGE_CLEANUP=${GENTOO_PORTAGE_CLEANUP:-'True'}
 export GENTOO_PYTHON_TARGETS=${GENTOO_PYTHON_TARGETS:-'python2_7 python3_6'}
 export GENTOO_PYTHON_ACTIVE_VERSION=${GENTOO_PYTHON_ACTIVE_VERSION:-'python3.6'}
 export GENTOO_OVERLAYS=${GENTOO_OVERLAYS:-''}
-export GENTOO_EMERGE_DEFAULT_OPTS=${GENTOO_EMERGE_DEFAULT_OPTS:-"--binpkg-respect-use --rebuilt-binaries=y --usepkg=y --with-bdeps=y --binpkg-changed-deps=y --quiet --jobs=2"}
+export GENTOO_EMERGE_DEFAULT_OPTS=${GENTOO_EMERGE_DEFAULT_OPTS:-"--binpkg-respect-use --rebuilt-binaries=y --usepkg=y --with-bdeps=y --binpkg-changed-deps=y --quiet --jobs=2 --autounmask=n"}
 
 # set the default bash array if GENTOO_EMERGE_ENV is not defined as an array
 if ! declare -p GENTOO_EMERGE_ENV  2> /dev/null | grep -q '^declare \-a'; then
-    export GENTOO_EMERGE_ENV=('USE="-build"' 'FEATURES="binpkg-multi-instance buildpkg parallel-fetch parallel-install"' 'EMERGE_DEFAULT_OPTS="--autounmask=n"')
+    declare -a GENTOO_EMERGE_ENV
+    GENTOO_EMERGE_ENV+=("USE=\"-build\"")
+    GENTOO_EMERGE_ENV+=("FEATURES=\"binpkg-multi-instance buildpkg parallel-fetch parallel-install\"")
+    GENTOO_EMERGE_ENV+=("PKGDIR=\"/tmp/portage-pkgdir\"")
+    GENTOO_EMERGE_ENV+=("DISTDIR=\"/tmp/portage-distdir\"")
+    GENTOO_EMERGE_ENV+=("PORTDIR=\"/tmp/portage-portdir\"")
+    export GENTOO_EMERGE_ENV
 fi
 # itterate over the array, exporting each 'line'
 for (( i=0; i<${#GENTOO_EMERGE_ENV[@]}; i++ )); do
