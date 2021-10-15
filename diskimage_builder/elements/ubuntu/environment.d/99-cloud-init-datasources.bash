d5fa64325fe3c2c4f662b717a80741382db50cc1
Ian Wienand <iwienand@redhat.com>
2021-10-15 16:40:46 +1100

    Revert "Allowing ubuntu element use local image"
    This reverts commit 1f4fb1d7a5709be3aa047430e698f6051887577f.
    
    This unfortunately wasn't actually tested.  Because the image-based
    tests run sequentially, a prior failure in the centos-8 job meant the
    ubuntu job never ran.
    
    This is failing with
    
     10-cache-ubuntu-tarball: line 28: DIB_LOCAL_IMAGE: unbound variable
    
    There is also a seemingly unused variable DIB_IMAGE_LOCAL_FILE; I'm
    not sure what this is doing.
    
    For now revert, and it can be re-proposed with appropriate testing.
    
    Change-Id: I0f3897c90dc863ee04c3295b9cb094f02d8658e3

--- diskimage_builder/elements/ubuntu/environment.d/99-cloud-init-datasources.bash
+++ diskimage_builder/elements/ubuntu/environment.d/99-cloud-init-datasources.bash
@@ -2,4 +2,3 @@
 # allowing the Ec2 data source from being queried on first boot, unless
 # specified otherwise.
 export DIB_CLOUD_INIT_DATASOURCES=${DIB_CLOUD_INIT_DATASOURCES:-"Ec2"}
-export DIB_IMAGE_LOCAL_FILE=${DIB_IMAGE_LOCAL_FILE:-}
