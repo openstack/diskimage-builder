6b42cb23eef0ca921ca933f354238f83a45229d8
Neil Hanlon <neil@shrug.pw>
2022-07-06 15:41:24 -0400

    Add Rockylinux 9 build configuration and update jobs for 8 and 9
    * Add note in Rocky Linux 9 containerfile to explain not needing to run
      systemd-machine-setup-id
    
    Change-Id: I01233ce27cc4558270b2f41fae36b46a17f56e52

--- diskimage_builder/elements/rocky-container/environment.d/10-01-rocky-distro-name.bash
+++ diskimage_builder/elements/rocky-container/environment.d/10-01-rocky-distro-name.bash
@@ -1,4 +1,4 @@
 export DISTRO_NAME=rocky
-export DIB_RELEASE=${DIB_RELEASE:-8} # always the most recent major
+export DIB_RELEASE=${DIB_RELEASE:-9} # always the most recent major
 export EFI_BOOT_DIR="EFI/rocky"
 export DIB_YUM_MINIMAL_CREATE_INTERFACES=0
